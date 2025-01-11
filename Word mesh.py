b13c2adef4d55ab06eaa4382864467ac56c06f74
saritpat <69256964+saritpat@users.noreply.github.com>
2025-01-12 01:44:05 +0700

    Word mesh

---
+++ Word mesh.py
@@ -0,0 +1,34 @@
+def word_mesh(words):
+    ans = ''
+    temp = ''
+    index = 0
+    for word in range(len(words)-1):
+        i = 0
+        while i < len(words[word]):
+            if words[word][i] == words[word+1][index]:
+                temp+=words[word][i]
+                index+=1
+                i+=1
+            elif words[word][i] == words[word+1][0] and index != 0:
+                temp=words[word][i]
+                index = 1
+                i+=1
+            else:
+                temp = ''
+                index = 0
+                i+=1
+        
+        if temp == '':
+            return 'failed to mesh'
+            
+        ans+=temp
+        temp = ''
+        index = 0
+
+    return ans
+
+
+print(word_mesh(["beacon", "condominium", "umbilical", "california"]))
+print(word_mesh(["allow", "lowering", "ringmaster", "terror"]))
+print(word_mesh(["abandon", "donation", "onion", "ongoing"]))
+print(word_mesh(["jamestown", "ownership", "hippocampus", "pushcart", "cartographer", "pheromone"]))
