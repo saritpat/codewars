2ac57d54f7793d87bf9a2a77c58160637d29e1e0
saritpat <69256964+saritpat@users.noreply.github.com>
2025-01-12 18:40:48 +0700

    Shorter sol. (use .join)

--- Mumbling.py
+++ Mumbling.py
@@ -1,13 +1,20 @@
-def accum(st):
-    ans = ''
-    for i in range(len(st)):
-        ans += st[i].upper()
-        ans += st[i].lower()*i
+# def accum(st):
+#     ans = ''
+#     for i in range(len(st)):
+#         ans += st[i].upper()
+#         ans += st[i].lower()*i
 
-        if i < len(st)-1:
-            ans += '-'
+#         if i < len(st)-1:
+#             ans += '-'
         
-    return ans
+#     return ans
+
+def accum(st):
+    ans = []
+    for i in range(len(st)):
+        ans.append(st[i].upper()+st[i].lower()*i)
+        
+    return '-'.join(ans)
 
 print(accum('ZpglnRxqenU'))
 print(accum('NyffsGeyylB'))
